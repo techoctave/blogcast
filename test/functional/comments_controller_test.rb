591fff3162f0ff971159a8598408b869cf628163
Tian Valdemar Davis <tiandavis@gmail.com>
2013-11-30 22:42:27 -0500

    Updated regression tests.

--- test/functional/comments_controller_test.rb
+++ test/functional/comments_controller_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
   test "the truth" do
     assert true
   end
--- test/functional/logins_controller_test.rb
+++ test/functional/logins_controller_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class LoginsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
   test "the truth" do
     assert true
   end
